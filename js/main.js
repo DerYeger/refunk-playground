4db2ce694ee9a1f9800e4c2f5c87006f966906e7
Jan Müller <janmueller3698@gmail.com>
2021-05-04 21:06:05 +0200

    feat: update content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+const recursivePlaceholder =
+  'val result = addition(40, 2)\n' +
+  'println("40 + 2 = $result")\n';
+
+const nonRecursivePlaceholder =
+  '// f(x, y) = 2 * (x + y)\n' +
+  'val f = multiplication(const(2), addition)\n' +
+  'val result = f(10, 11)\n' +
+  'println("2 * (10 + 11) = $result")\n';
+
 class RefunkPlayground extends HTMLElement {
   connectedCallback() {
-    const useRecursion = this.getAttribute('use-recursion')
+    KotlinPlayground('refunk-playground');
+
+    this.setAttribute('highlight-on-fly', true.toString());
+    this.setAttribute('theme', 'darcula');
+
+    if (this.getAttribute('data-highlight-only') !== null) {
+      return;
+    }
+
+    const useRecursion = this.getAttribute('use-recursion') === 'true';
+    const sample = useRecursion ? recursivePlaceholder : nonRecursivePlaceholder;
+
     this.innerHTML =
       'import eu.yeger.refunk.base.*\n' +
       'import eu.yeger.refunk.exception.*\n' +
@@ -8,12 +29,13 @@ class RefunkPlayground extends HTMLElement {
       '\n' +
       'fun main() {\n' +
       '//sampleStart\n' +
-      'println(addition(40, 2))\n' +
+      sample +
+      '\n' +
+      '\n' +
+      '\n' +
       '//sampleEnd\n' +
-      '}'
-
-    KotlinPlayground('refunk-playground')
+      '}';
   }
 }
 
-window.customElements.define('refunk-playground', RefunkPlayground)
+window.customElements.define('refunk-playground', RefunkPlayground);
